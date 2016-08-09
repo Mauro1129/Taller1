c543d203c74ae7e4d63e4539c5f358bb8a6b25a0
Mauro1129 <mauro8929@gmail.com>
2016-08-08 20:41:59 -0500

    creacion proyecto 1

---
+++ Hola+mundo.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[4]:
+
+print "Hola Mundo"
+
+
+# $\int_x^\infty\frac{dt}{t(t^2-1)\log{(t)}}$
+
+# In[6]:
+
+2+4*6/8.0-3
+
+
+# In[8]:
+
+3/2
+
+
+# In[10]:
+
+3.0/2.0
+
+
+# In[12]:
+
+3+5j
+
+
+# In[15]:
+
+(3+5j)-(1-1j)
+
+
+# In[17]:
+
+abs(3+5j)
+
+
+# In[21]:
+
+int(_17)
+
+
+# In[23]:
+
+float(_21)
+
+
+# In[25]:
+
+complex(_23)
+
+
+# In[28]:
+
+x,y,z=1,2,3
+
+
+# In[35]:
+
+print x,y,z
+
+
+# In[38]:
+
+print (x);print(y);print(z)
+
+
+# In[50]:
+
+if z>y:
+    print "El Mayor es",z
+else:
+    print "El Mayor es",y
+
